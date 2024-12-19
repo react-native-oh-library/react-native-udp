c6fbfe4d0b610ea2f678a5cf2ae203aa21ae01ca
Qinlian1 <168181884+Qinlian1@users.noreply.github.com>
2024-12-19 15:28:49 +0800

    chore: 开源五项核查整改 (#12)
    Co-authored-by: qinlianao <qinlianao@h-partners.com>

--- harmony/react_native_udp/src/main/ets/RNUdpPackage.ts
+++ harmony/react_native_udp/src/main/ets/RNUdpPackage.ts
@@ -22,6 +22,12 @@
  * SOFTWARE.
  */
 
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { RNPackage, TurboModulesFactory } from "@rnoh/react-native-openharmony/ts";
 import type { TurboModule, TurboModuleContext } from "@rnoh/react-native-openharmony/ts";
 import { TM } from "@rnoh/react-native-openharmony/generated/ts";
