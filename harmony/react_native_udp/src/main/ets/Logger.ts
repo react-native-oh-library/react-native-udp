0dfb8d83782df0e15269e07de904d26ef72c181f
qinlianao <qinlianao@h-partners.com>
2024-12-19 15:15:23 +0800

    chore: 开源五项核查整改

--- harmony/react_native_udp/src/main/ets/Logger.ts
+++ harmony/react_native_udp/src/main/ets/Logger.ts
@@ -22,6 +22,12 @@
  * SOFTWARE.
  */
 
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import hilog from '@ohos.hilog';
 
 class Logger {
